e462df7e10089fded04741edd0fb9da6782b291d
anuragdongre <anuragdongre555@gmail.com>
2023-06-04 23:54:10 +0530

    React todo app Q11

---
+++ React_Todo_App_Q11/react_todo_app_q11/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import TodoApp from './Components/TodoApp';
+
+function App() {
+  return (
+   <TodoApp></TodoApp>
+  );
+}
+
+export default App;
